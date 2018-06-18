a231ab098513e0eafff9e8c846e71220b15b79cd
Benoit Letondor <benoit.letondor@gmail.com>
2018-06-18 14:14:42 +0200

    Initial commit

---
+++ app/src/main/java/com/benoitletondor/democollapsingtoolbarscrollbug/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.benoitletondor.democollapsingtoolbarscrollbug
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        // Make the header fullscreen (- status bar height)
+        val layoutParams = header_content.layoutParams
+        layoutParams.height =
+            resources.displayMetrics.heightPixels - resources.getDimensionPixelSize(resources.getIdentifier("status_bar_height", "dimen", "android"))
+    }
+}
